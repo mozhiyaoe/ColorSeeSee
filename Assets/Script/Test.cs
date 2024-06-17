e25912376b11a3755e6eaf22548efa82db57c6e1
mozhiyaoe <1282281240@qq.com>
2024-06-17 20:04:47 +0800

    2024-6-17

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = Color.black;
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
