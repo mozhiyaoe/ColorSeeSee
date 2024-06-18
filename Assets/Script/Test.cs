8e31a17f6e8ecd305782ff2526c7754cae3f822c
mozhiyaoe <1282281240@qq.com>
2024-06-18 17:26:02 +0800

    2024-6-18-5

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -2,18 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
+    //倒计时开始数字
+    public float timeleft;
+    //文本先是倒计时
+    public Text CountdownText;
+    //是否开始倒计时
+    private bool isCounting;
     void Start()
     {
         gameObject.GetComponent<SpriteRenderer>().color = Color.black;
+         //开始倒计时
+        isCounting = true;
 
         // Update is called once per frame
         void Update()
         {
+            //判定是否开始倒计时
+            if (isCounting)
+            {
+                timeleft -= Time.deltaTime;
 
+            }
+            //文本倒计时先是数字
+            CountdownText.text = timeleft.ToString(format: "0.00");
+            //当倒计时小于等于0时，停止倒计时
+            if (timeleft <= 0 && isCounting)
+            {
+                //停止倒计时
+                isCounting = false;
+            }
         }
     }
 }
